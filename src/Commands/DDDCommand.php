e071ec628b84e5aa6d45c8bb098ad528421b5b28
caioDeodato <dev.caiolima@gmail.com>
2022-02-03 11:38:25 -0300

    climbing ddd base command

--- src/Commands/DDDCommand.php
+++ src/Commands/DDDCommand.php
@@ -6,17 +6,60 @@ use Illuminate\Console\Command;
 
 class DDDCommand extends Command
 {
-    protected $signature = 'make:ddd {paste}';
+   /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:ddd';
 
-    protected $description = 'Cria o sistema de pastas e arquivos para Application';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create default DDD structure';
 
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct();
     }
 
+    protected function getDirectoryName()
+    {
+        return config('dddartisan.base_path');
+    }
+
+    protected function resolve($pathname)
+    {
+        mkdir(base_path('/') . $pathname);
+        mkdir(base_path("/$pathname/Domain"));
+        mkdir(base_path("/$pathname/Application"));
+        mkdir(base_path("/$pathname/Support"));
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
     public function handle()
     {
-        echo 'DDD Command';
+        $rootDDD = $this->getDirectoryName();
+        $this->resolve($rootDDD);
+
+        echo "\n\033[" . '0;32m' . "DDD structure has been created" . "\033[0m";
+        echo "\n\nDo not forget to add these paths to composer.json in autoload < psr-4:\n";
+        echo "    {\n";
+        echo "        \"Application\\\\\": \"$rootDDD/Application\",\n";
+        echo "        \"Domain\\\\\": \"$rootDDD/Domain\",\n";
+        echo "        \"Support\\\\\": \"$rootDDD/Support\",\n";
+        echo "    }\n";
+
     }
 }
