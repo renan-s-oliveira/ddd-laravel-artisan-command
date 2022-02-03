db9b516a09f600ef6ce783e0ff0327305081bd3d
Renan de Souza Oliveira <renanoliveira1988@gmail.com>
2022-02-03 09:19:51 -0300

    add new collaborator

---
+++ src/Commands/DDDCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DDDArtisan\Commands;
+
+use Illuminate\Console\Command;
+
+class ApplicationCommand extends Command
+{
+    protected $signature = 'make:application {name} {type}';
+
+    protected $description = 'Cria o sistema de pastas e arquivos para Application';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        echo 'Application Command';
+    }
+}
