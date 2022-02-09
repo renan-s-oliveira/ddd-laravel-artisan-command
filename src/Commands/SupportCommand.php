28b3d9d2eacf05f23a3163e8fcb56e9d370dc2c5
caioDeodato <dev.caiolima@gmail.com>
2022-02-09 17:25:23 -0300

    feat: create support command

--- src/Commands/SupportCommand.php
+++ src/Commands/SupportCommand.php
@@ -3,20 +3,127 @@
 namespace DDDArtisan\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 
 class SupportCommand extends Command
 {
-    protected $signature = 'make:support';
+    protected $signature = 'make:support {name}';
 
     protected $description = 'Cria o sistema de pastas e arquivos para o support';
 
-    public function __construct()
+    /**
+     * Filesystem instance
+     * @var Filesystem
+     */
+    protected $files;
+
+    /**
+     * Create a new command instance.
+     * @param Filesystem $files
+     */
+    public function __construct(Filesystem $files)
     {
         parent::__construct();
+
+        $this->files = $files;
     }
 
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
     public function handle()
     {
-        echo 'Support Command';
+        $path = $this->getSourceFilePath();
+        $this->files->isDirectory(dirname($path)) or $this->files->makeDirectory(dirname($path), 0777, true);
+
+        $contents = $this->getSourceFile();
+
+        if (!$this->files->exists($path)) {
+            $this->files->put($path, $contents);
+            $this->info('Support ' . $this->getSingularClassName($this->argument('name')) . ' created successfully.');
+        } else {
+            $this->info("File : {$path} already exits");
+        }
+    }
+
+    protected function getDirectoryName()
+    {
+        return config('dddartisan.base_path');
+    }
+
+    /**
+     * Return the Singular Capitalize Name
+     * @param $name
+     * @return string
+     */
+    public function getSingularClassName($name)
+    {
+        return ucwords($name);
+    }
+
+    /**
+     * Return the stub file path
+     * @return string
+     *
+     */
+    public function getStubPath()
+    {
+        return __DIR__ . '/../../../stubs/support/support.stub';
+    }
+
+    /**
+     **
+     * Map the stub variables present in stub to its value
+     *
+     * @return array
+     *
+     */
+    public function getStubVariables()
+    {
+        return [
+            'NAMESPACE'         => "Support",
+            'CLASS_NAME'        => $this->getSingularClassName($this->argument('name')),
+        ];
+    }
+
+    /**
+     * Get the stub path and the stub variables
+     *
+     * @return bool|mixed|string
+     *
+     */
+    public function getSourceFile()
+    {
+        return $this->getStubContents($this->getStubPath(), $this->getStubVariables());
+    }
+
+    /**
+     * Replace the stub variables(key) with the desire value
+     *
+     * @param $stub
+     * @param array $stubVariables
+     * @return bool|mixed|string
+     */
+    public function getStubContents($stub, $stubVariables = [])
+    {
+        $contents = file_get_contents($stub);
+
+        foreach ($stubVariables as $search => $replace) {
+            $contents = str_replace('$' . $search . '$', $replace, $contents);
+        }
+
+        return $contents;
+    }
+
+    /**
+     * Get the full path of generate class
+     *
+     * @return string
+     */
+    public function getSourceFilePath()
+    {
+        return base_path($this->getDirectoryName() . "/Support") . '/' . $this->getSingularClassName($this->argument('name')) . '.php';
     }
 }
