b36c36445d05167e59d5fbb13e459c4e1a2c6f3c
caioDeodato <dev.caiolima@gmail.com>
2022-02-14 20:56:53 -0300

    climb domain

--- src/Commands/DomainCommand.php
+++ src/Commands/DomainCommand.php
@@ -3,10 +3,11 @@
 namespace DDDArtisan\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 
 class DomainCommand extends Command
 {
-    protected $signature = 'make:domain';
+    protected $signature = 'make:domain {name}';
 
     protected $description = 'Cria o sistema de pastas e arquivos para o domínio';
 
@@ -17,6 +18,30 @@ class DomainCommand extends Command
 
     public function handle()
     {
-        echo 'Domain Command';
+        $this->resolve();
+    }
+
+    protected function getDirectoryName()
+    {
+        return config('dddartisan.base_path');
+    }
+
+    /**
+     * Return the Singular Capitalize Name
+     * @param $name
+     * @return string
+     */
+    public function getSingularClassName($name)
+    {
+        return ucwords($name);
+    }
+
+    protected function resolve()
+    {
+        $name = $this->getSingularClassName($this->argument('name'));
+
+        Artisan::call('make:domain-dto ' . $name . " " . $name . "Data");
+        Artisan::call('make:domain-action ' . $name . " " . "Create$name");
+        Artisan::call('make:domain-model ' . $name . " " . $name);
     }
 }
